1b823b3f59fb44a6a5c86990d59956723719ae89
FHHF <fabianhumbertohernandez@gmail.com>
2020-03-04 20:55:58 +0200

    first commit

---
+++ src/test/java/org/dev/fhhf/SinglePageApp/SinglePageAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dev.fhhf.SinglePageApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SinglePageAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
