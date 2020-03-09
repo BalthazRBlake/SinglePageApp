b0f62f6fa9eb0536f7455e8de79f7959475be6ef
FHHF <fabianhumbertohernandez@gmail.com>
2020-03-09 20:19:45 +0200

    fix REST endPoint

--- src/main/java/org/dev/fhhf/SinglePageApp/resources/EmployeeResource.java
+++ src/main/java/org/dev/fhhf/SinglePageApp/resources/EmployeeResource.java
@@ -3,15 +3,13 @@ package org.dev.fhhf.SinglePageApp.resources;
 import org.dev.fhhf.SinglePageApp.model.Employee;
 import org.dev.fhhf.SinglePageApp.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@RestController
 @CrossOrigin
-@RestController("/spapp")
+@RequestMapping("/spapp")
 public class EmployeeResource {
 
     @Autowired
